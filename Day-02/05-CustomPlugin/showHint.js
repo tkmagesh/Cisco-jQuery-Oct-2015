e7653499fb9b2442f37a12094ff4af25ee284313
tkmagesh <tkmagesh77@gmail.com>
2015-10-06 16:29:10 +0530

    Day-2 Complete

---
+++ Day-02/05-CustomPlugin/showHint.js
@@ -0,0 +1,25 @@
+$.fn.showHint = function(options){
+    options = options || {};
+    var hintAttr = options.hintAttr || 'hintText';
+    $(this).each(function(index, element){
+
+        var $element = $(element),
+            hintText = $element.attr(hintAttr);
+
+        $element.focus(function(){
+            if ($element.val() === hintText){
+                $element
+                    .val('')
+                    .removeClass('hint')
+            }
+        });
+        $element.blur(function(){
+            if ($element.val() === ''){
+                $element
+                    .val(hintText)
+                    .addClass('hint')
+            }
+        });
+        $element.blur();
+    });
+}
